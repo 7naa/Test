dcf7a9db563224061e22eb70777ba07e8ec69d32
yna <b022210245@student.utem.edu.my>
2025-01-26 23:08:01 +0800

    5000

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT||8181;
+const port = process.env.PORT||5000;
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
